c85d40e14aae17cbf63ab8d24763a85a7388fcbe
mobone <nicholas.reichel@gmail.com>
2018-01-26 13:36:10 -0600

    Export all data now uses all columns

--- utility/export_all_data.py
+++ utility/export_all_data.py
@@ -8,16 +8,16 @@ db_collection = db['coborns_products']
 df = pd.DataFrame(list(db_collection.find({})))
 
 print(df.columns)
-columns = ['Product Name', 'Product Price', 'Product Size', 'Product Price Per Unit', 'Servings Per Container ',
+"""columns = ['Product Name', 'Product Price', 'Product Size', 'Product Price Per Unit', 'Servings Per Container ',
            'Calories ', 'Total Fat ', 'Saturated Fat ', 'Cholesterol ', 'Sodium ',
            'Total Carbohydrate ', 'Dietary Fiber ', 'Protein ', 'Vitamin A ',
-           'Vitamin C ', 'Calcium ', 'Iron ']
-df = df[columns]
+           'Vitamin C ', 'Calcium ', 'Iron ']"""
+#df = df[columns]
 
 #df['Product Price'] = df['Product Price'].str.replace(' SALE','')
 #df['Product Price'] = df['Product Price'].replace('$','')
 
-for i in df.columns[1:]:
+for i in df.columns:
     print(i)
     try:
         df[i] = pd.to_numeric(df[i])
@@ -26,6 +26,6 @@ for i in df.columns[1:]:
 #df['Price Per Serving'] = df['Price'] / df['Servings Per Container']
 #df['Price'] = df['Price Per Serving']
 #del df['Price Per Serving']
-df = df.fillna(0)
+#df = df.fillna(0)
 print(df)
 df.to_csv('test_data.csv', index=False)
